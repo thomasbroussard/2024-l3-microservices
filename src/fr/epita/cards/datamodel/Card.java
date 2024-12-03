753337e3f2ce2db6f9f37591fb0df6280a4a80d4
Présentation <tbr@tbr.com>
2024-12-03 12:16:39 +0100

    first integration

---
+++ src/fr/epita/cards/datamodel/Card.java
@@ -0,0 +1,35 @@
+package fr.epita.cards.datamodel;
+
+public class Card {
+    private int value;
+    private String color;
+
+    public Card(int value, String color) {
+        this.value = value;
+        this.color = color;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "value=" + value +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
