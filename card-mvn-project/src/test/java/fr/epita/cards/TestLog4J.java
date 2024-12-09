abb1fba2215c5245be9ac694b9aca6f6b2ff958e
Présentation <tbr@tbr.com>
2024-12-09 12:50:22 +0100

    adding : log4j for logging

--- card-mvn-project/src/test/java/fr/epita/cards/TestLog4J.java
+++ card-mvn-project/src/test/java/fr/epita/cards/TestLog4J.java
@@ -2,13 +2,24 @@ package fr.epita.cards;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class TestLog4J {
     private static final Logger LOGGER = LogManager.getLogger(TestLog4J.class);
 
+    @BeforeAll
+    public static void globalSetUp(){
+        LOGGER.info("beforeAll");
+    }
+
+    @BeforeEach
+    public void setUp(){
+        LOGGER.info("beforeEach");
+    }
+
 
     @Test
+    @DisplayName("REQ-001 This test shows how log4j is working")
     public void test(){
 
        LOGGER.error("when an error occurs");
@@ -18,4 +29,15 @@ public class TestLog4J {
        LOGGER.trace("maximum details");
 
     }
+
+    @AfterEach
+    public void cleanUp(){
+        LOGGER.info("afterEach");
+    }
+
+    @AfterAll
+    public static void globalCleanUp(){
+        LOGGER.info("afterAll");
+    }
+
 }
