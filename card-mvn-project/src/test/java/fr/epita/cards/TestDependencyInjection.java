4549726ae25b203fce7b09178d2f31e3f027c29b
Présentation <tbr@tbr.com>
2025-01-07 13:06:42 +0100

    updated spring test

---
+++ card-mvn-project/src/test/java/fr/epita/cards/TestDependencyInjection.java
@@ -0,0 +1,42 @@
+package fr.epita.cards;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ApplicationConfiguration.class)
+public class TestDependencyInjection {
+
+    private static final Logger LOGGER = LogManager.getLogger(TestDependencyInjection.class);
+
+    @Autowired
+    DataSource dataSource;
+
+
+    @Test
+    public void test() throws SQLException {
+        //when
+        Connection connection = dataSource.getConnection();
+
+        //then
+        Assertions.assertThat(connection).isNotNull();
+        Assertions.assertThat(connection.getSchema()).isEqualTo("PUBLIC");
+
+        LOGGER.info(connection.getSchema());
+
+
+    }
+
+}
